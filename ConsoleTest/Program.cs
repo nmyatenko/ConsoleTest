5f8880ff073d1b55386ad12873af2fa65c4950cf
nmyatenko <jamalrs@mail.ru>
2014-12-23 11:47:35 +0200

    Test commit

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello world!");
+            Console.WriteLine("Hello world!");
+
         }
     }
 }
